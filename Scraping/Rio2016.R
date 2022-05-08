f3b979c15455981112c03dfe09539d4ba576a416
leirom1990 <104939560+leirom1990@users.noreply.github.com>
2022-05-08 19:14:25 +0100

    Add files via upload
    web Scraping

---
+++ Scraping/Rio2016.R
@@ -0,0 +1,7 @@
+library(htmltab)
+url_dani <- "https://en.wikipedia.org/wiki/2016_Summer_Olympics_medal_table"
+variable_to_save_table <- htmltab(doc=url_dani, which=3)
+variable_to_save_table
+head(variable_to_save_table, 10)
+str(variable_to_save_table)
+write.table(variable_to_save_table, file = "data.xls", sep="\t", row.names=FALSE)
